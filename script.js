721af6d7cd861e91ba92e48a4b031bfa1f8cee07
Luke <mackenzieluke94@gmail.com>
2019-02-21 20:11:33 -0500

    luke commit - started 3rd ajax call

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ app.getUserInput = function() {
   app.getSimilarArtists(userInput);
 }
 
+//this method returns the top 10 similar artist results to the user's input
 app.getSimilarArtists = function(artist){
   $.ajax({
     url: app.url,
@@ -32,24 +33,32 @@ app.getSimilarArtists = function(artist){
     }
   }).then(function (result) {
     // console.log(result.similarartists.artist);
+    //this variable stores the artist array
     const artistMatch = result.similarartists.artist;
     // console.log(artistMatch)
+    //this variable is created from the filter method on the artist match array. the filter loops through each array and only returns the ones that have an mbid(artist id)
    const artistIdFilter = artistMatch.filter((artist) => {
       return artist.mbid;
     })
 
+    //this variable is an array of ids of the artists that had ids
    const artistIds = artistIdFilter.map((id) => {
       return id.mbid;
     })
+
+    // console.log(artistIds);
+
+    //each id is passed through as an argument in the getTopAlbum method.
     artistIds.forEach((id) => {
-      app.getAlbumInfo(id);
+      app.getTopAlbum(id);
     })
     // console.log(artistIds);
   })
 }
 
 //this method gets similiar artist recommendations album info
-app.getAlbumInfo = function(id){
+// this id refers to the artist id
+app.getTopAlbum = function(id){
   $.ajax({
     url: app.url,
     data: {
@@ -60,10 +69,30 @@ app.getAlbumInfo = function(id){
       limit: 1
     }
   }).then(function(result){
-    console.log(result.topalbums);
-    const albumArt = 
+    console.log(result);
+    return result;
   })
 }
+
+//this method is to get the album info of the top album for each artist. we need to pass the mbid of the top album as the argument in this method
+//using the album id from GetTopAlbums, pass through GetTopAlbumInfo to get cover art, track list, etc.
+app.getTopAlbumInfo = function(id) {
+  $.ajax({
+    url: app.url,
+    data: {
+      api_key: app.apiKey,
+      method: 'album.getinfo',
+      format: 'json',
+      mbid: id,
+      limit: 1
+    }
+  }).then(function (result) {
+    console.log(result);
+    return result;
+  })
+}
+
+
 
 // app.getAlbumInfo("38f59974-2f4d-4bfa-b2e3-d2696de1b675");
 
