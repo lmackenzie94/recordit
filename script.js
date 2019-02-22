212f34e33813cc9debd92e9ecf72a979a8d9bfd6
Luke <mackenzieluke94@gmail.com>
2019-02-21 21:26:43 -0500

    minor changes/added pseudo code - luke

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ $('form').on('submit', function (event) {
   app.getUserInput();
 })
 
-app.getUserInput = function() {
+app.getUserInput = function () {
   userInput = $('#search').val();
   // console.log(userInput);
   app.getSimilarArtists(userInput);
 }
 
 //this method returns the top 10 similar artist results to the user's input
-app.getSimilarArtists = function(artist){
+app.getSimilarArtists = function (artist) {
   $.ajax({
     url: app.url,
     data: {
@@ -37,12 +37,12 @@ app.getSimilarArtists = function(artist){
     const artistMatch = result.similarartists.artist;
     // console.log(artistMatch)
     //this variable is created from the filter method on the artist match array. the filter loops through each array and only returns the ones that have an mbid(artist id)
-   const artistIdFilter = artistMatch.filter((artist) => {
+    const artistIdFilter = artistMatch.filter((artist) => {
       return artist.mbid;
     })
 
     //this variable is an array of ids of the artists that had ids
-   const artistIds = artistIdFilter.map((id) => {
+    const artistIds = artistIdFilter.map((id) => {
       return id.mbid;
     })
 
@@ -58,52 +58,53 @@ app.getSimilarArtists = function(artist){
 
 //this method gets similiar artist recommendations album info
 // this id refers to the artist id
-app.getTopAlbum = function(id){
+app.getTopAlbum = function (artistId) {
   $.ajax({
     url: app.url,
     data: {
       api_key: app.apiKey,
       method: 'artist.gettopalbums',
       format: 'json',
-      mbid: id,
+      mbid: artistId,
       limit: 1
     }
-  }).then(function(result){
-    console.log(result);
-    return result;
+  }).then(function (result) {
+    const topAlbumID = result.topalbums.album[0].mbid;
+    // console.log(topAlbumID);
+    app.getTopAlbumInfo(topAlbumID);
   })
 }
 
 //this method is to get the album info of the top album for each artist. we need to pass the mbid of the top album as the argument in this method
 //using the album id from GetTopAlbums, pass through GetTopAlbumInfo to get cover art, track list, etc.
-app.getTopAlbumInfo = function(id) {
+app.getTopAlbumInfo = function (albumId) {
   $.ajax({
     url: app.url,
     data: {
       api_key: app.apiKey,
       method: 'album.getinfo',
       format: 'json',
-      mbid: id,
-      limit: 1
+      mbid: albumId,
+      // limit: 1
     }
   }).then(function (result) {
-    console.log(result);
-    return result;
+    console.log(result.album.name);
   })
 }
 
+//the getSimilarArtists function returns 10 artists similar to the artist the user inputted
+//the artistIds is an array of just the mbid's of those 10 artists
+//the getTopAlbum function takes an argument of artistID and returns the 1 top album of each artist (the ID's are passed into it one by one using a forEach loop)
+//the getTopAlbum function returns the album mbid of the top album from each artist, which is then passed into the getTopAlbumInfo function.
+//the getTopAlbumInfo function returns an object of info for each top album.
 
 
-// app.getAlbumInfo("38f59974-2f4d-4bfa-b2e3-d2696de1b675");
 
-//
+app.init = function () {
 
-
-app.init = function() {
-  
 }
 
 //document ready
-$(function(){
-  app.init();  
+$(function () {
+  app.init();
 })
