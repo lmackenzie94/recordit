8fab6de51c68b29218c9c9f864fa51ebfca83016
Luke <mackenzieluke94@gmail.com>
2019-02-24 18:50:30 -0500

    final commit

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-const app = {};
+const app = {}; 
 
-//Alanna's API key
 app.apiKey = "aa0e1ce1cb622fb5704728e8b09bd4bd";
 app.url = "http://ws.audioscrobbler.com/2.0/";
 
+// global variable to store user input.
 let userInput = '';
 
-//method to store user's input into the user input variable
-
-$('form').on('submit', function (event) {
-  event.preventDefault();
-  app.getUserInput();
-  app.getSimilarArtists(userInput); //moved to here
-  app.albumIdArray = [];
-  app.finalArray = app.albumIdArray;
-  $('.recordWall').empty();
-  // $('footer').css('opacity', '1');
-})
-
-app.getUserInput = function () {
+//method to store user's input into the user input variable.
+app.getUserInput = function() {
   userInput = $('#search').val();
-  // app.getSimilarArtists(userInput);
 }
 
-app.getSimilarArtists = function (artist) {
-
+// method that returns no more than 15 similar artists based on the user input.
+app.getSimilarArtists = function(artist) {
   const similarArtists = $.ajax({
     url: app.url,
     data: {
@@ -45,19 +33,21 @@ app.getSimilarArtists = function (artist) {
       app.artistIds = artistIdFilter.map((id) => {
         return id.mbid;
       })
-      // console.log(artistIds);
+      
+      // passes each artist ID as argument into getTopAlbum method.
       app.artistIds.forEach((id) => {
         app.getTopAlbum(id);
       })
 
+      // error handling - if user input has no results, append this to the page.
       if (artistMatch.length === 0) {
         $('.recordWall').append(`<p class="noResults">Sorry friend, no matching records for ${userInput}.</p>`)
       }
-      
 
     })
 }
 
+// method that returns each similar artists' top album ID.
 app.getTopAlbum = function (artistId) {
 
   const topAlbums = $.ajax({
@@ -75,12 +65,14 @@ app.getTopAlbum = function (artistId) {
 
       const albumId = result.topalbums.album[0].mbid;
 
-        if (albumId) {
-          app.getTopAlbumInfo(albumId);
-        }
+      // passes each top album ID as argument into getTopAlbumInfo method.
+      if (albumId) {
+        app.getTopAlbumInfo(albumId);
+      }
     })
 }
 
+// method that returns info for each top album.
 app.getTopAlbumInfo = function (albumId) {
 
   const albumInfo = $.ajax({
@@ -95,15 +87,16 @@ app.getTopAlbumInfo = function (albumId) {
   })
   $.when(albumInfo)
   .then((result) => {
-    // console.log(result);
     app.topAlbumInfo = result;
     
+    // variables to store various album details.
     const albumCover = app.topAlbumInfo.album.image[3]['#text'];
     const albumName = app.topAlbumInfo.album.name;
     const artistName = app.topAlbumInfo.album.artist;
     const artistSearchName = artistName.replace(/\s/g, '%20');
     const albumSearchName = albumName.replace(/\s/g, '%20');
     
+    // information to be displayed on the page.
     $('.recordWall').append(`
     <div class="record" tabindex="0" role="button" aria-pressed="false">
       <img src=${albumCover} alt='${app.topAlbumInfo.album.name} album cover.'>
@@ -114,89 +107,93 @@ app.getTopAlbumInfo = function (albumId) {
         <span class="close">&times;</span>
         <p><span class="bold">Album:</span> ${albumName}</p>
         <p><span class="bold">Artist:</span> ${artistName}</p>
-        <a href=https://open.spotify.com/search/results/${artistSearchName}%20${albumSearchName} target="_blank"><i class="fab fa-spotify"></i>Listen on Spotify</a>
+        <a href=https://open.spotify.com/search/results/${artistSearchName}%20${albumSearchName} target="_blank" aria-label="Listen on Spotify"><i class="fab fa-spotify" aria-hidden="true"></i><span>Listen on Spotify</span></a>
       </div>
     </div>
     `)
   })
 }
 
-$('.recordWall').on('click', '.record', function() {
-  console.log(this);
-  $(this).next().css('display', 'block');
-})
+app.init = function() {
+  
+  // listen for form submit
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    app.getUserInput();
+    app.getSimilarArtists(userInput);
+    $('.recordWall').empty();
+  })
 
-$('.recordWall').on('keydown', '.record', function(e) {
-  if (e.keyCode === 13) {
+  // event handlers 
+  $('.recordWall').on('click', '.record', function () {
+    console.log(this);
     $(this).next().css('display', 'block');
-  }
-})
+  })
 
-$('.recordWall').on('click', '.close', function() {
-  $('.modal').css('display', 'none');
-})
+  $('.recordWall').on('keydown', '.record', function (e) {
+    if (e.keyCode === 13) {
+      $(this).next().css('display', 'block');
+    }
+  })
 
+  $('.recordWall').on('click', '.close', function () {
+    $('.modal').css('display', 'none');
+  })
 
-$('.recordWall').on('click', '.modal', function() {
-  $('.modal').css('display', 'none');
-})
+  $('.recordWall').on('click', '.modal', function () {
+    $('.modal').css('display', 'none');
+  })
 
-$('button').on('click', function() {
-  $('.themeModal').css('display', 'block');
-})
+  $('button').on('click', function () {
+    $('.themeModal').css('display', 'block');
+  })
 
+  $('.close').on('click', function () {
+    $('.themeModal').css('display', 'none');
+  })
 
-$('.close').on('click', function() {
-  $('.themeModal').css('display', 'none');
-})
+  $('.themeModal').on('click', function () {
+    $('.themeModal').css('display', 'none');
+  })
 
-$('.themeModal').on('click', function() {
-  $('.themeModal').css('display', 'none');
-})
-
-$('.themeOne').on('click', function() {
-  $('body').attr('class',"");
-  $('body').addClass('theme1');
-})
-
-$('.themeTwo').on('click', function () {
-  $('body').attr('class', "");
-  $('body').addClass('theme2');
-})
-
-$('.themeThree').on('click', function () {
-  $('body').attr('class', "");
-  $('body').addClass('theme3');
-})
-
-$('.themeOne').on('keydown', function (e) {
-  if (e.keyCode === 13) {
+  $('.themeOne').on('click', function () {
     $('body').attr('class', "");
     $('body').addClass('theme1');
-    $('.themeModal').css('display', 'none');
-  }
-})
+  })
 
-$('.themeTwo').on('keydown', function (e) {
-  if (e.keyCode === 13) {
+  $('.themeTwo').on('click', function () {
     $('body').attr('class', "");
     $('body').addClass('theme2');
-    $('.themeModal').css('display', 'none');
-  }
-})
+  })
 
-$('.themeThree').on('keydown', function (e) {
-  if (e.keyCode === 13) {
+  $('.themeThree').on('click', function () {
     $('body').attr('class', "");
     $('body').addClass('theme3');
-    $('.themeModal').css('display', 'none');
-  }
-})
+  })
 
+  $('.themeOne').on('keydown', function (e) {
+    if (e.keyCode === 13) {
+      $('body').attr('class', "");
+      $('body').addClass('theme1');
+      $('.themeModal').css('display', 'none');
+    }
+  })
 
+  $('.themeTwo').on('keydown', function (e) {
+    if (e.keyCode === 13) {
+      $('body').attr('class', "");
+      $('body').addClass('theme2');
+      $('.themeModal').css('display', 'none');
+    }
+  })
 
-app.init = function () {
-
+  $('.themeThree').on('keydown', function (e) {
+    if (e.keyCode === 13) {
+      $('body').attr('class', "");
+      $('body').addClass('theme3');
+      $('.themeModal').css('display', 'none');
+    }
+  })
 }
 
 //document ready
